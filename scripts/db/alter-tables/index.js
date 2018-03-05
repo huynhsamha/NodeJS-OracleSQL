f7403407212f64ca46701b57ce7d60c744e88d7f
huynhsamha <huynhsamha@gmail.com>
2018-03-05 10:58:15 +0700

    add script constraint department

--- scripts/db/alter-tables/index.js
+++ scripts/db/alter-tables/index.js
@@ -3,7 +3,13 @@ const path = require('path');
 
 const data = {
   EMPLOYEES:
-    fs.readFileSync(path.join(__dirname, './ALTER-TABLE-EMPLOYEES.sql')).toString()
+    fs.readFileSync(path.join(__dirname, './ALTER-TABLE-EMPLOYEES.sql')).toString(),
+
+  DEPARTMENTS:
+    fs.readFileSync(path.join(__dirname, './ALTER-TABLE-DEPARTMENTS.sql')).toString()
 };
 
-module.exports = String().concat(data.EMPLOYEES);
+module.exports = String().concat(
+  data.EMPLOYEES,
+  data.DEPARTMENTS
+);
